b0ca625d5931ab4047bf4bd9f336df0ce294d63b
Subhrakanti <subhrokanti97@gmail.com>
2024-01-18 22:29:56 +0530

    Add profile modal and signals

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -44,7 +44,8 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        user.save(self._db)
+        user.save(using=self._db)
+        return user
     
     def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
         """
