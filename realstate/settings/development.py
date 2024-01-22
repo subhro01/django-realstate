4049942a0f37bbaaabdf2411e3b0f1c0c744d8f2
Subhrakanti <subhrokanti97@gmail.com>
2024-01-22 20:48:55 +0530

    implement JWT and Djoser for authentication and autorization with mailtrap

--- realstate/settings/development.py
+++ realstate/settings/development.py
@@ -1,5 +1,15 @@
 from .base import *
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DAFAULT_FROM_EMAIL = "no-reply@realestatedev.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
+
 DATABASES = {
     "default": {
         "ENGINE": env("POSTGRES_ENGINE"),
